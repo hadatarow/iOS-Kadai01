46a472381816f0991cb1bda8f8cba988193dbb99
羽田良太朗 <>
2019-05-18 14:47:00 +0900

    first commit

---
+++ iOS Kadai01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Kadai01
+//
+//  Created by 羽田良太朗 on 2019/05/09.
+//  Copyright © 2019 Ryotaro Hada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
